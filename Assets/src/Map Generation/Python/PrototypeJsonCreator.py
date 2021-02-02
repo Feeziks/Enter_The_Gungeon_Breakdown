0b81697107a987aed89c8c436f3dfa6a47de4026
Feeziks <feeziksg@gmail.com>
2021-02-01 19:25:01 -0700

    Created python script to generate prototype information

--- Assets/src/Map Generation/Python/PrototypeJsonCreator.py
+++ Assets/src/Map Generation/Python/PrototypeJsonCreator.py
@@ -1,5 +1,13 @@
 from PIL import Image
 import os
+import json
+
+
+def convert_list_to_dict(a):
+    it = iter(a)
+    res_dct = dict(zip(it, it))
+    return res_dct
+
 
 if __name__ == "__main__":
     #Get all images in the grand-parent directory
@@ -10,6 +18,8 @@ if __name__ == "__main__":
     for file in os.listdir(path):
         if file.endswith(".png"):
             files.append(file)
+
+    dictionary = {}
 
     #Go through the images and get the pixels in the corners and centers
     for file in files:
@@ -26,4 +36,39 @@ if __name__ == "__main__":
         southWest   = pixels[0                      , 0]
         west        = pixels[0                      , halfY]
         northWest   = pixels[0                      , thisSprite.size[1] - 1]
+
+        prototypename = "prototype_" + os.path.splitext(file)[0]
+
+        dictionary[prototypename] = {
+            'sprite' : file,
+            'sockets' : {
+                'north': north,
+                'northeast': northEast,
+                'east': east,
+                'southeast': southEast,
+                'south': south,
+                'southwest': southWest,
+                'west': west,
+                'northwest': northWest
+            }
+        }
+
+    directions =            ["north", "northeast", "east", "southeast", "south", "southwest", "west", "northwest"]
+    directions_reverse =    ["south", "southwest", "west", "northwest", "north", "northeast", "east", "southeast"]
+
+    for this_prototype, _ in dictionary.items():
+        neighbor_list = {}
+        for i in directions:
+            neighbor_list[i] = []
+        for other_prototype, _ in dictionary.items():
+            if(this_prototype != other_prototype):
+                for idx, _ in enumerate(directions):
+                    if(dictionary[this_prototype]["sockets"][directions[idx]] == dictionary[other_prototype]["sockets"][directions_reverse[idx]]):
+                        neighbor_list[directions[idx]].append(other_prototype)
+                        
+        dictionary[this_prototype]["neighbor_list"] = neighbor_list
         
+
+    with open('test.txt', 'w') as outfile:
+        json.dump(dictionary, outfile, indent=4)
+    
