f75c7a9f06b052c2f86a7dabaed87e35c8958d2c
Feeziks <feeziksg@gmail.com>
2021-02-09 22:19:24 -0700

    Messed up the git history by merging this branch into master incorrectly

--- Assets/src/Map Generation/Map.cs
+++ Assets/src/Map Generation/Map.cs
@@ -7,6 +7,8 @@ public class Map
     //This class holds the data that describes the map of any given level
     //This class does not perform any actions relating to generating/deleting/checking etc. maps it is just a data holder
 
+    //Messed up the git history by accident so making a change ehre so I can make a new commit
+
     //Constructor
     public Map()
     {
